ce5d1275531e1eacde134da853a30bdb71e18581
BATUHAN UZUNOGLU <batuhan.uzunoglu@ext.digiturk.com.tr>
2023-02-05 01:43:51 +0300

    Add project files.

---
+++ XUnitTutorial/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitTutorial
+{
+    public class Calculator
+    {
+        //private CalculatorState _state = CalculatorState.Cleared;
+        public decimal Value { get; private set; } = 0;
+        public decimal Add(decimal value)
+        {
+            return Value += value;
+        }
+    }
+}
---
+++ XUnitTutorial/CalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitTutorial
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _sut; // System under test
+
+        public CalculatorTests()
+        {
+            _sut = new Calculator();
+        }
+
+        [Fact]
+        public void AddTwoNumbersShouldEqualTheirEqual()
+        {
+            _sut.Add(5);
+            _sut.Add(6);
+            Assert.Equal(11, _sut.Value);
+        }
+
+        [Theory]
+        [InlineData(5,2,3)]
+        [InlineData(0,0,0)]
+        [InlineData(-2,-1,-1)]
+        public void AddTwoNumbersShouldEqualTheirEqualTheory(
+            decimal expected, decimal firstToAdd, decimal secondToAdd)
+        {
+            _sut.Add(firstToAdd);
+            _sut.Add(secondToAdd);
+            Assert.Equal(expected, _sut.Value);
+        }
+    }
+}
